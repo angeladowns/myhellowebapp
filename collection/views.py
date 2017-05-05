777119aa90199db96e7aa0bbd42dc68f8b25d4f9
angeladowns <angeladowns@me.com>
2017-05-05 18:00:23 +0900

    Ch6 Add film icon and remove template tag examples from Ch5.

--- collection/views.py
+++ collection/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from collection.models import Film
 
 # Create your views here.
 def index(request):
+    films = Film.objects.all()
+    return render(request, 'index.html', {
+    'films': films,
+    })
     # defining the variable
     number = 6
     thing = "Thing name"
     # this is your new view
     return render(request, 'index.html', {
-    'number': number,
-    'thing': thing,
+        'number': number,
+        'thing': thing,
     })
