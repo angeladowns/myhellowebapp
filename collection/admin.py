1347731585e734bc2fa5d3b06fd66f6d8a3ae911
angeladowns <angeladowns@me.com>
2017-05-05 17:05:23 +0900

    Ch6 Add admin model.

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+# import your model
+from collection.models import Film
+# set up automated slug creation
+class FilmAdmin(admin.ModelAdmin):
+    model = Film
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
 
+admin.site.register(Film, FilmAdmin)
 # Register your models here.
