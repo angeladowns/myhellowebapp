83e061a4ea833e407ca629beb8973bcb5c72b4bf
angeladowns <angeladowns@me.com>
2017-05-05 18:46:42 +0900

    Ch8 add links to individual film pages.

--- collection/views.py
+++ collection/views.py
@@ -7,3 +7,9 @@ def index(request):
     return render(request, 'index.html', {
     'films': films,
     })
+
+def film_detail(request, slug):
+    film = Film.objects.get(slug=slug)
+    return render(request, 'films/film_detail.html', {
+        'film': film,
+    })
